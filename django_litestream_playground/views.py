95a248f6423be47f139316205428836aabc0eb15
TAKAHASHI Shuuji <shuuji3@gmail.com>
2025-01-05 14:06:32 +0900

    feat: show list of index table names too

--- django_litestream_playground/views.py
+++ django_litestream_playground/views.py
@@ -9,7 +9,8 @@ from django.shortcuts import render
 def home(request: HttpRequest):
     now = datetime.datetime.now()
     tables = get_all_table_names()
-    return render(request, "home.html", {"now": now, "tables": tables})
+    index_tables = get_all_index_table_names()
+    return render(request, "home.html", {"now": now, "tables": tables, "index_tables": index_tables})
 
 
 def get_all_table_names() -> [str]:
@@ -17,9 +18,21 @@ def get_all_table_names() -> [str]:
     try:
         with closing(sqlite3.connect('db.sqlite3')) as conn:
             with closing(conn.cursor()) as cursor:
-                cursor.execute("SELECT * FROM sqlite_master WHERE type='table';")
-                table_names = [row for row in cursor.fetchall()]
+                cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+                table_names = [row[0] for row in cursor.fetchall()]
                 return table_names
+    except sqlite3.Error as e:
+        print(f"Error accessing database: {e}")
+        return []
+
+def get_all_index_table_names() -> [str]:
+    """Gets a list of all index table names."""
+    try:
+        with closing(sqlite3.connect('db.sqlite3')) as conn:
+            with closing(conn.cursor()) as cursor:
+                cursor.execute("SELECT name FROM sqlite_master WHERE type='index';")
+                index_table_names = [row[0] for row in cursor.fetchall()]
+                return index_table_names
     except sqlite3.Error as e:
         print(f"Error accessing database: {e}")
         return []
